ef92101949cfdeba4adfb2a921b803349a1d6c47
dhambure98 <akila789liyanage@gmail.com>
2022-07-06 00:32:30 +0530

    creating_HomePage_component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react"
+
+class HomePage extends Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Hello From the Home Page</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage
