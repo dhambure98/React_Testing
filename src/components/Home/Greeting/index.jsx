76ab9f3b728eadac9613331c50a61f7f97194ac5
dhambure98 <akila789liyanage@gmail.com>
2022-07-06 02:30:59 +0530

    install_mui_styling_sheet

--- src/components/Home/Greeting/index.jsx
+++ src/components/Home/Greeting/index.jsx
@@ -1,6 +1,8 @@
 import { Component } from "react"
 import Typography from '@mui/material/Typography';
 import backgroundIMG from "../../../assets/img/backgroundIMG.png"
+import {styleSheet} from "./style";
+import {withStyles} from "@mui/styles";
 
 class Greeting extends Component {
     constructor(props) {
@@ -8,8 +10,11 @@ class Greeting extends Component {
     }
 
     render() {
+
+        const{classes} = this.props;
         return (
-            <div>
+            /*<div style={{backgroundColor: 'yellow'}}>  --> don't use like this */
+            <div className={classes.container}>
                 <div>
                     <Typography variant="h3" gutterBottom component="div">
                         Hello Akila {this.props.name}
@@ -17,11 +22,11 @@ class Greeting extends Component {
                 </div>
                 <div>
                     <img src="https://imgs.search.brave.com/vorjX8TUhAwKZZ_Vqwlq0pBaWD3pRJjMqeZaIiTVYro/rs:fit:742:167:1/g:ce/aHR0cHM6Ly93d3cu/b3UuYWMubGsvd3At/Y29udGVudC91cGxv/YWRzLzIwMTkvMDYv/b3VzbC1sb2dvMi5w/bmc" alt="https://www.ijse.lk"/>
-                    <img src={backgroundIMG} alt="https://www.ijse.lk"/>
+                    {/*<img src={backgroundIMG} alt="https://www.ijse.lk"/> --> this line also working */}
                 </div>
             </div>
         );
     }
 }
 
-export default Greeting
+export default withStyles(styleSheet) (Greeting)
